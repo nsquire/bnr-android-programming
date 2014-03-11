8d972cd7961e45d0ed4f400575c6989dd33a5032
Nicholas Squire <nsquire@visa.com>
2014-03-11 07:15:37 -0700

    Finishing up chapter 2.

---
+++ GeoQuiz/app/src/main/java/com/nick/geoquiz/app/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.nick.geoquiz.app;
+
+/**
+ * Created by nsquire on 3/10/14.
+ */
+public class TrueFalse {
+
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        this.mQuestion = question;
+        this.mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        this.mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        this.mTrueQuestion = trueQuestion;
+    }
+}
