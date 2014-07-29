aa8188ef45eee35734fd4c64e9273f2fcfc21d4b
Squire, Nicholas <nsquire@visa.com>
2014-07-29 16:22:33 -0700

    Finishing up second challenge from chapter 13 using SurfaceView.

--- HelloMoon/app/src/main/java/com/nick/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/com/nick/android/hellomoon/HelloMoonFragment.java
@@ -3,7 +3,10 @@ package com.nick.android.hellomoon;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,10 +18,12 @@ import android.widget.Button;
 public class HelloMoonFragment extends Fragment {
 
 
-    private AudioPlayer mPlayer = new AudioPlayer();
+    private VideoPlayer mPlayer = new VideoPlayer();
     private Button mPlayButton;
     private Button mPauseButton;
     private Button mStopButton;
+    private SurfaceView mVideoSurfaceView;
+    private SurfaceHolder mVideoSurfaceHolder;
 
 
     public HelloMoonFragment() {
@@ -32,11 +37,31 @@ public class HelloMoonFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 
+        mVideoSurfaceView = (SurfaceView) v.findViewById(R.id.helloMoon_videoSurfaceView);
+        mVideoSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Log.d("HelloMoonFragment", "surfaceCreated()");
+                mVideoSurfaceHolder = holder;
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                Log.d("HelloMoonFragment", "surfaceChanged()");
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                Log.d("HelloMoonFragment", "surfaceDestroyed()");
+            }
+        });
+
         mPlayButton = (Button) v.findViewById(R.id.helloMoon_playButton);
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mPlayer.play(getActivity());
+                Log.d("HelloMoonFragment", "Created holder:" + mVideoSurfaceHolder.toString());
+                mPlayer.play(getActivity(), mVideoSurfaceHolder);
             }
         });
 
@@ -55,6 +80,7 @@ public class HelloMoonFragment extends Fragment {
                 mPlayer.stop();
             }
         });
+
         return v;
     }
 
