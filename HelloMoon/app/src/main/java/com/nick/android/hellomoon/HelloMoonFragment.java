ebc3a1ce97a44be02a7219f2d4912114d578f952
Squire, Nicholas <nsquire@visa.com>
2014-07-29 17:27:50 -0700

    Finishing up chapter 14.

--- HelloMoon/app/src/main/java/com/nick/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/com/nick/android/hellomoon/HelloMoonFragment.java
@@ -26,6 +26,13 @@ public class HelloMoonFragment extends Fragment {
 
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
