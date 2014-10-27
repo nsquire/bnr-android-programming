e2f1916e2c813381fb251831a96cf28dccc19379
Squire, Nicholas <nsquire@visa.com>
2014-10-27 16:23:01 -0700

    Finishing up chapter 23.

---
+++ NerdLauncher/app/src/main/java/com/nick/android/nerdLauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package com.nick.android.nerdLauncher;
+
+import android.support.v4.app.Fragment;
+
+
+public class NerdLauncherActivity extends FragmentHostingActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new NerdLauncherFragment();
+    }
+}
---
+++ NerdLauncher/app/src/main/java/com/nick/android/nerdLauncher/NerdLauncherFragment.java
@@ -0,0 +1,128 @@
+package com.nick.android.nerdLauncher;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NerdLauncherFragment extends Fragment implements AbsListView.OnItemClickListener {
+    private static final String TAG = "NerdLauncherFragment";
+
+    /**
+     * The fragment's ListView/GridView.
+     */
+    private AbsListView mListView;
+
+    /**
+     * The Adapter which will be used to populate the ListView/GridView with
+     * Views.
+     */
+    private ListAdapter mAppsArrayAdapter;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public NerdLauncherFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager packageManager = getActivity().getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "I've found " + activities.size() + " activities");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo lhs, ResolveInfo rhs) {
+                PackageManager packageManager = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        lhs.loadLabel(packageManager).toString(),
+                        rhs.loadLabel(packageManager).toString());
+            }
+        });
+
+        // Change Adapter to display your content
+
+        mAppsArrayAdapter = new ArrayAdapter<ResolveInfo>(getActivity(), android.R.layout.simple_list_item_1, activities) {
+            public View getView(int position, View convertView, ViewGroup parent) {
+                PackageManager packageManager = getActivity().getPackageManager();
+                View v = super.getView(position, convertView, parent);
+                // Documentation says that simple_list_item_1 is a TextView,
+                // so cast it so that you can set it text value
+                TextView textView = (TextView) v;
+                ResolveInfo resolveInfo = getItem(position);
+                textView.setText(resolveInfo.loadLabel(packageManager));
+                return v;
+            }
+        };
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item, container, false);
+
+        // Set the adapter
+        mListView = (AbsListView) view.findViewById(android.R.id.list);
+        ((AdapterView<ListAdapter>) mListView).setAdapter(mAppsArrayAdapter);
+
+        // Set OnItemClickListener so we can be notified on item clicks
+        mListView.setOnItemClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ResolveInfo resolveInfo = (ResolveInfo) parent.getAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (null == activityInfo) {
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName(activityInfo.packageName, activityInfo.name);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    /**
+     * The default content for this Fragment has a TextView that is shown when
+     * the list is empty. If you would like to change the text, call this method
+     * to supply the text it should use.
+     */
+    public void setEmptyText(CharSequence emptyText) {
+        View emptyView = mListView.getEmptyView();
+
+        if (emptyView instanceof TextView) {
+            ((TextView) emptyView).setText(emptyText);
+        }
+    }
+
+
+}
